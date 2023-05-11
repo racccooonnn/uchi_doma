0e3e63dbd99c8037449059451a9a91b072ac480c
Arina <129192952+racccooonnn@users.noreply.github.com>
2023-05-11 17:34:26 +0300

    Add files via upload

---
+++ практика 2/MathClient/MathClient/MathClient.cpp
@@ -0,0 +1,17 @@
+﻿
+#include <iostream>
+#include "MathLibrary.h"
+
+int main()
+{
+    fibonacci_init(1, 1);
+    do {
+        std::cout << fibonacci_index() << ": "
+            << fibonacci_current() << std::endl;
+    } while (fibonacci_next());
+    std::cout << fibonacci_index() + 1 <<
+        " Fibonacci sequence values fit in an " <<
+        "unsigned 64-bit integer." << std::endl;
+}
+
+
